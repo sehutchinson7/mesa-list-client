a0734afac86345b7d3e99d9ed929adbc691ad8bd
sehutchinson7 <sehutchinson7@gmail.com>
2018-04-25 23:53:21 -0400

    Cleared create, update, delete forms on submit by invoking onClearAnimals

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -43,7 +43,7 @@ const onSignOut = function (event) {
   // console.log('sign out ran')
   api.signOut()
     .then(ui.signOutSuccess)
-    // clears the list after a sign out so next user who signs in starts with no list being displayed
+    // clear the table after a sign out so next user who signs in starts with no list being displayed
     .then(onClearAnimals())
     .catch(ui.signOutFailure)
 }
@@ -60,7 +60,8 @@ const onAddAnimal = function (event) {
   const data = getFormFields(event.target)
   api.addAnimal(data)
     .then(ui.addAnimalSuccess)
-    //.then(() => onGetAnimals()) on successful creation of animal, automatically adds to databa
+    // clear the table after an animal is added
+    .then(onClearAnimals())
     .catch(ui.addAnimalFailure)
 }
 
@@ -70,6 +71,8 @@ const onUpdateAnimal = function (event) {
   console.log(data)
   api.updateAnimal(data)
     .then(ui.updateAnimalSuccess)
+    // clear the table after an animal is updated
+    .then(onClearAnimals())
     .catch(ui.updateAnimalFailure)
 }
 
@@ -84,6 +87,8 @@ const onDeleteAnimal = function (event) {
   console.log(data)
   api.deleteAnimal(data)
     .then(ui.deleteAnimalSuccess)
+    // clear the table after an animal is deleted
+    .then(onClearAnimals())
     .catch(ui.deleteAnimalFailure)
 }
 
